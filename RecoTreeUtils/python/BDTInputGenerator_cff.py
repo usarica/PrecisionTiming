f566689056b3af0ff80f33d756de86880eac3345
Simone Pigazzi <simone.pigazzini@live.it>
2017-02-17 00:59:03 +0100

    Adds gitignore

--- RecoTreeUtils/python/BDTInputGenerator_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-BDTInputGenerator = cms.EDAnalyzer("BDTInputGenerator",
-    ## analyzed particles type: photons
-    particleType = cms.untracked.int32(4),
-    ## matrix side
-    matrixSide = cms.untracked.int32(3)
-)
--- RecoTreeUtils/rootlogon.C
+++
@@ -1,8 +0,0 @@
-{
-	gSystem->Load("libFWCoreFWLite.so"); 
-	AutoLibraryLoader::enable();
-	gSystem->Load("libDataFormatsFWLite.so");
-	gSystem->Load("libDataFormatsPatCandidates.so");
-	
-#include "DataFormats/FWLite/interface/Handle.h"
-}
